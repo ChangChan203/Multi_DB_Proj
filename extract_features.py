3710821a20ba40df83691894795154cd88a252c1
Chang Chan <131477689+ChangChan203@users.noreply.github.com>
2025-05-14 16:53:46 +0700

    Update extract_features.py

--- extract_features.py
+++ extract_features.py
@@ -5,7 +5,7 @@ import traceback
 def pre_emphasis(signal, coeff=0.97):
     return np.append(signal[0], signal[1:] - coeff * signal[:-1])
 
-def extract(file_path, sr=16000, n_mfcc=13, frame_length_ms=25, hop_length_ms=10, window='hamming'):
+def extract(file_path, sr=16000, n_mfcc=13, frame_length_ms=25, hop_length_ms=10, window='hamming', global_mean=None, global_std=None):
     try:
         y, _ = librosa.load(file_path, sr=sr)
         y = pre_emphasis(y, coeff=0.97)
@@ -22,7 +22,12 @@ def extract(file_path, sr=16000, n_mfcc=13, frame_length_ms=25, hop_length_ms=10
 
         mean = np.mean(mfcc_39, axis=1)
         std = np.std(mfcc_39, axis=1)
-        feature_vector = np.concatenate([mean, std])
+        feature_vector = np.concatenate([mean, std])  # 78 chiều
+
+        # ✅ Z-score normalization
+        if global_mean is not None and global_std is not None:
+            feature_vector = (feature_vector - global_mean) / global_std
+
         return feature_vector
     except Exception as e:
         print(f"[Lỗi extract()] File: {file_path}")
